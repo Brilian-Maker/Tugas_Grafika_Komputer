5d7394f49ae0108c1f4fd6dc621835d8f37c0a1e
Brilian-Maker <134251637+Brilian-Maker@users.noreply.github.com>
2024-09-27 00:17:18 +0700

    Add files via upload

---
+++ tugas.js
@@ -0,0 +1,10 @@
+function setup() {
+  createCanvas(640, 480);
+}
+
+function draw() {
+  background(120, 532, 3);
+  ellipse(200,180,200,130)
+  ellipse(440,180,200,130)
+  ellipse(440,300,200,130)
+}
